11ce652615d8c4787fff08422c52940bbedbe5cf
Alexandre Ribeiro <vribeiro.alexandre@gmail.com>
2017-04-30 17:20:45 -0300

    Bundlers - using webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,17 @@
 import express  from 'express';
 import path  from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.dev';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(webpackConfig);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+}));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
